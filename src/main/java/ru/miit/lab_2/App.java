87abf1fc4c3646244e05c94dcac2585f38cd8754
DrOne <43498102+imdrone@users.noreply.github.com>
2020-11-04 10:53:15 +0300

    init app

---
+++ src/main/java/ru/miit/lab_2/App.java
@@ -0,0 +1,1 @@
+package ru.miit.lab_2;import org.jboss.weld.environment.se.Weld;import org.jboss.weld.environment.se.WeldContainer;public class App {    public static void main(String[] args) {        Weld weld = new Weld();        WeldContainer container = weld.initialize();        System.out.println("Hello world");        container.shutdown();    }}
